2bea240f9126a34951a857eb9fe25f604d785388
aashishhhhhh <47654151+aashishhhhhh@users.noreply.github.com>
2021-03-16 22:42:34 +0545

    createTwoActivitiesProject/Task1

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -8,7 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchSecondActivity(View view) {
-
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
