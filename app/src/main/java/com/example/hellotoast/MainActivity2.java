7d594d0acf912afa1cd3f9be678b4dbe4ff9d964
aashishhhhhh <47654151+aashishhhhhh@users.noreply.github.com>
2021-03-17 15:07:50 +0545

    hello_toast_hw

---
+++ app/src/main/java/com/example/hellotoast/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.hellotoast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        int mCount = intent.getIntExtra(MainActivity.EXTRA_COUNT, 0);
+        TextView mCountTextView = findViewById(R.id.count_text);
+        mCountTextView.setText(String.valueOf(mCount));
+    }
+}
